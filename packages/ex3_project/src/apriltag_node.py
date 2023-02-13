bff24618bac4b79767b8fdb0fe6970286a2dca5f
liqianxi <qianxi@ualberta.ca>
2023-02-12 17:22:14 -0700

    finish 1.2

--- packages/ex3_project/src/apriltag_node.py
+++ packages/ex3_project/src/apriltag_node.py
@@ -11,8 +11,10 @@ from cv_bridge import CvBridge
 from duckietown_utils import load_homography, load_map
 import rospkg 
 from dt_apriltags import Detector, Detection
+from duckietown_msgs.msg import Twist2DStamped, WheelEncoderStamped, WheelsCmdStamped,LEDPattern
+from duckietown_msgs.srv import SetCustomLEDPattern, ChangePattern
 # Code from https://github.com/Coral79/exA-3/blob/44adf94bad728507608086b91fbf5645fc22555f/packages/augmented_reality_basics/include/augmented_reality_basics/augmented_reality_basics.py
-
+# https://docs.photonvision.org/en/latest/docs/getting-started/pipeline-tuning/apriltag-tuning.html
 class AprilTagNode(DTROS):
 
     def __init__(self, node_name):
@@ -33,7 +35,8 @@ class AprilTagNode(DTROS):
         # Publishersrospack
         
         self.pub_result_ = rospy.Publisher(f'/{self.veh}/apriltag_node/modified/image/compressed', CompressedImage,queue_size=1)
-    
+
+        self.current_led_pattern = 4
 
         # extract parameters from camera_info_dict for apriltag detection
         f_x = self.camera_info_dict['camera_matrix']['data'][0]
@@ -48,11 +51,17 @@ class AprilTagNode(DTROS):
         self.at_detector = Detector(searchpath=['apriltags'],
                            families='tag36h11',
                            nthreads=1,
-                           quad_decimate=1.0,
+                           quad_decimate=0.7,
                            quad_sigma=0.0,
                            refine_edges=1,
                            decode_sharpening=0.25,
                            debug=0)
+
+        serve_name = f"{self.veh}/led_emitter_node/set_custom_pattern"
+        rospy.wait_for_service(serve_name)
+
+        self.emitter_service = rospy.ServiceProxy(serve_name, SetCustomLEDPattern,persistent=True)
+
     def projection_matrix(self, K, H):
         """
             K is the intrinsic camera matrix
@@ -84,7 +93,103 @@ class AprilTagNode(DTROS):
         P = K @ buff
 
         return P
-    
+
+    def color_pattern(self,mode):
+        msg = LEDPattern()
+        if mode == 1:
+            msg.color_list = ['red', 'red', 'red', 'red', 'red']
+            msg.color_mask = [0, 0, 0, 0, 0]
+            msg.frequency = 0
+            msg.frequency_mask = [0, 0, 0, 0, 0]
+        elif mode==2:
+            msg.color_list = ['blue', 'blue', 'blue', 'blue', 'blue']
+            msg.color_mask = [0, 0, 0, 0, 0]
+            msg.frequency = 0
+            msg.frequency_mask = [0, 0, 0, 0, 0]
+        elif mode==3:
+            msg.color_list = ['green', 'green', 'green', 'green', 'green']
+            msg.color_mask = [0, 0, 0, 0, 0]
+            msg.frequency = 0
+            msg.frequency_mask = [0, 0, 0, 0, 0]
+        elif mode==4:
+            msg.color_list = ['white', 'white', 'white', 'white', 'white']
+            msg.color_mask = [0, 0, 0, 0, 0]
+            msg.frequency = 0
+            msg.frequency_mask = [0, 0, 0, 0, 0]
+
+        return msg
+
+    def change_led(self, tag_id):
+
+        tag_id = int(tag_id)
+        if tag_id in [63, 143, 58, 62, 133, 153]:
+            # T-intersection
+            if self.current_led_pattern != 2:
+                self.emitter_service(self.color_pattern(2))
+                self.current_led_pattern = 2
+        elif tag_id in [162,169]:
+            # Stop sign
+            if self.current_led_pattern != 1:
+                self.emitter_service(self.color_pattern(1))
+                self.current_led_pattern = 1
+        elif tag_id in [93,94,200,201]:
+            # U of A sign
+            if self.current_led_pattern != 3:
+                self.emitter_service(self.color_pattern(3))
+                self.current_led_pattern =3
+
+    def draw_boundary(self,corners, image):
+        """
+        corners = [[375.08706665 209.4493866 ]
+            [445.62255859 212.48104858]
+            [453.69393921 136.50134277]
+            [377.13027954 138.41127014]]
+        
+        """
+        color = (0, 0, 255)
+        x_list, y_list = [],[]
+        for i in range(len(corners)-1):
+            start = (int(corners[i][0]),int(corners[i][1]))
+            end = (int(corners[i+1][0]),int(corners[i+1][1]))
+            cv2.line(image, start, end, color, 5)
+        
+        cv2.line(image, (int(corners[0][0]),int(corners[0][1])), 
+                        (int(corners[-1][0]),int(corners[-1][1])), color, 5)
+        return image
+
+    def add_text_to_img(self, tag_id, center, img):
+        font                   = cv2.FONT_HERSHEY_SIMPLEX
+
+        fontScale              = 0.5
+        fontColor              = (255,255,255)
+        thickness              = 1
+        lineType               = 2
+
+
+        if tag_id in [63, 143, 58, 62, 133, 153]:
+            # T-intersection
+            text = "T-intersection"
+
+        elif tag_id in [162,169]:
+            # Stop sign
+            text = "Stop sign"
+
+        elif tag_id in [93,94,200,201]:
+            # U of A sign
+            text = "U of A"
+
+
+        cv2.putText(img,text, 
+            (int(center[0]),int(center[1])), 
+            font, 
+            fontScale,
+            fontColor,
+            thickness,
+            lineType)
+
+        return img
+
+
     def project(self, msg):
         br = CvBridge()
 
@@ -97,7 +202,7 @@ class AprilTagNode(DTROS):
         # 133 and 153 are left and right T.
         # 94, 93, 201,200 are u of a logos :use green color
         # otherwise, use white color.
-        print("tags",tags)
+
         """
         [Detection object:
             tag_family = b'tag36h11'
@@ -135,20 +240,36 @@ class AprilTagNode(DTROS):
                     
                     
         """
-        for tag in tags:
-            H = tag.homography # assume only 1 tag in image
+        new_img = dis
+        if len(tags) == 0:
+            if self.current_led_pattern != 4:
+                self.emitter_service(self.color_pattern(4))
+                self.current_led_pattern = 4
+            
+        else:
+            for tag in tags:
+                print("decision_margin",tag.decision_margin)
+                if tag.decision_margin < 20:
+                    continue
+                H = tag.homography # assume only 1 tag in image
 
-            # Find transformation from april tag to target image frame
-            P = self.projection_matrix(self.K, H)
+                # Find transformation from april tag to target image frame
+                P = self.projection_matrix(self.K, H)
+                
+                self.change_led(tag.tag_id)
 
-            # Project model into image frame
-            #image_np = self.renderer.render(image_np, P)
+                corners = tag.corners
+
+                new_img = self.draw_boundary(corners, dis)
+                new_img = self.add_text_to_img(tag.tag_id, tag.center, new_img)
+                # Project model into image frame
+                #image_np = self.renderer.render(image_np, P)
         
         # make new CompressedImage to publish
         augmented_image_msg = CompressedImage()
         augmented_image_msg.header.stamp = rospy.Time.now()
         augmented_image_msg.format = "jpeg"
-        augmented_image_msg.data = np.array(cv2.imencode('.jpg', dis)[1]).tostring()
+        augmented_image_msg.data = np.array(cv2.imencode('.jpg', new_img)[1]).tostring()
         #render = self.augmenter.render_segments(points=self._points, img=dis, segments=self._segments)
         #result = br.cv2_to_compressed_imgmsg(render,dst_format='jpg')
         self.pub_result_.publish(augmented_image_msg)
